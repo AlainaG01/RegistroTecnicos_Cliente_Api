8f286608bee3736e4dbf4f372272672c925062fb
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-10-31 21:11:13 -0400

    Add project files.

---
+++ Tecnicos.Data/Models/Clientes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnicos.Data.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClienteId { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras")]
+    public string? WhatsApp { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+}
---
+++ Tecnicos.Domain/DTO/ClientesDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnicos.Domain.DTO;
+
+public class ClientesDto
+{
+    public int ClientesId { get; set; }
+    public string? Nombres { get; set; }
+    public string? WhapsApp { get; set; }
+}
